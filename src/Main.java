97e1390598c4120fee6c9580b382b74cc7bef401
Brevex <BrenoB99@Protonmail.com>
2023-11-06 00:22:46 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,31 @@
 
 */
 
+import binarysearchtree.BinaryTree;
+import binarysearchtree.Node;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        BinaryTree tree = new BinaryTree();
+
+        tree.insertElement(10);
+        tree.insertElement(5);
+        tree.insertElement(15);
+        tree.insertElement(3);
+        tree.insertElement(7);
+        tree.insertElement(14);
+        tree.insertElement(16);
+        tree.insertElement(2);
+        tree.insertElement(4);
+        tree.insertElement(13);
+
+        Node root = tree.getRoot();
+        int leftSize = tree.getLeftSubtreeSize(root);
+        int rightSize = tree.getRightSubtreeSize(root);
+
+        System.out.println("Left size: " + leftSize);
+        System.out.println("Right size: " + rightSize);
     }
 }
